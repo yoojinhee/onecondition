34710142fd786f7a7cea2f89b48a8fc0944b136b
yoojinhee <52348335+yhujinhui@users.noreply.github.com>
2020-05-06 15:34:23 +0900

    논리연산자 실습

--- onecondition/Program.cs
+++ onecondition/Program.cs
@@ -10,6 +10,12 @@ namespace onecondition
     {
         static void Main(string[] args)
         {
+            Console.Write("x :");
+            int x = int.Parse(Console.ReadLine());
+            if (x > 10 && x < 20)
+            {
+                Console.WriteLine("조건에 일치합니다");
+            }
         }
     }
 }
